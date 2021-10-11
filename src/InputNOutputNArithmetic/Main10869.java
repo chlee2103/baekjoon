706039fd5f5d6bc2619416a31573cd5de827ef7b
chlee2103 <rhdqnx@naver.com>
2021-10-11 18:39:52 +0900

    10869-사칙연산

---
+++ src/InputNOutputNArithmetic/Main10869.java
@@ -0,0 +1,16 @@
+package InputNOutputNArithmetic;
+
+import java.util.Scanner;
+
+public class Main10869 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
