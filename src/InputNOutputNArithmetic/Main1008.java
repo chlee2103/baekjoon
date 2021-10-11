ae089f80414a3548e9bde65a9ca87350c2d8cc94
chlee2103 <rhdqnx@naver.com>
2021-10-11 18:38:13 +0900

    1008-A/B

---
+++ src/InputNOutputNArithmetic/Main1008.java
@@ -0,0 +1,12 @@
+package InputNOutputNArithmetic;
+
+import java.util.Scanner;
+
+public class Main1008 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double a = scn.nextDouble();
+        double b = scn.nextDouble();
+        System.out.println(a/b);
+    }
+}
