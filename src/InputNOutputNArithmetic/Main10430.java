821e823e1c8b851a169f34b1298192447b1dbc35
chlee2103 <rhdqnx@naver.com>
2021-10-11 18:43:39 +0900

    10430-나머지

---
+++ src/InputNOutputNArithmetic/Main10430.java
@@ -0,0 +1,16 @@
+package InputNOutputNArithmetic;
+
+import java.util.Scanner;
+
+public class Main10430 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
